e1b54d676e5b7c09740b0db31cbc1e1c26608e23
Memas1112 <161666193+Memas1112@users.noreply.github.com>
2024-07-15 20:01:50 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload() {
+}
+
+Function setup(){
+    canvas = createcanvas(640,480);
+    canvas.position(110,250);
+    video = createCapture(video);
+    video.hide();
+
+    Tint_color = "";
+}
